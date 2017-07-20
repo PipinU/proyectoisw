1077aa6ce37d9ba9452794b202d27eabdfa59349
Felipe Ignacio Gonzalez Ceballos <feigonza@alumnos.ubiobio.cl>
2017-07-20 12:51:15 -0400

    modificacion em migracion record

--- database/migrations/2017_07_20_020534_create_records_table.php
+++ database/migrations/2017_07_20_020534_create_records_table.php
@@ -16,7 +16,7 @@ class CreateRecordsTable extends Migration
             $table->increments('id');
             $table->text('descripcion');
             $table->integer('id_order')->unsigned();
-            $table->foreign('id_order')->references('id')->on('order')->onDelete('cascade');
+            $table->foreign('id_order')->references('id')->on('orders')->onDelete('cascade');
             $table->timestamps();
         });
     }
